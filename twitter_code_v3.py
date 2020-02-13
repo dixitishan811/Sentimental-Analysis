af86ebf23c95f5ff60c3eaab546f120f36e368f9
IamMarcIvanov <45270390+IamMarcIvanov@users.noreply.github.com>
2020-02-13 14:52:58 +0530

    Update twitter_code_v3.py

--- twitter_code_v3.py
+++ twitter_code_v3.py
@@ -1,5 +1,3 @@
-# ADDED SPELL CORRECTION
-
 import nltk.sentiment.vader as sent
 import pandas as pd
 from sklearn.metrics import confusion_matrix
@@ -14,64 +12,52 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
 from sklearn import tree
 from sklearn.feature_extraction.text import TfidfVectorizer
 import gensim
 from gensim.models import Word2Vec
 from textblob import TextBlob
 
+
 class Data:
     def __init__(self):
         self.analyzer = sent.SentimentIntensityAnalyzer()
 
         self.raw_tweets = None
-        self.get_tweets()
-        self.raw_tweets = self.raw_tweets[:100000]
 
         self.train_labels = None
         self.test_labels = None
-      # self.create_label(self.train_tweets, self.test_tweets)
 
+        self.get_tweets()
 
     def get_tweets(self):
-        raw_train = pd.read_csv('training.1600000.processed.noemoticon.csv', header=None,
+        raw_train = pd.read_csv('training.1600000.processed.noemoticon.csv',
+                                header=None,
                                 names=['Score', 'A', 'B', 'C', 'D', 'Tweet'])
         self.raw_tweets = pd.Series(raw_train['Tweet'], dtype='str')
 
-
-
-
-    def create_label(self, train_tweet_sequence, test_tweet_sequence):
+    def create_train_label(self, train_tweet_sequence):
         score = []
-
         for tweet in train_tweet_sequence:
-
-            x = self.analyzer.polarity_scores(tweet)['compound']
-            score.append(round(x))
-
-
+            val = self.analyzer.polarity_scores(tweet)['compound']
+            score.append(round(val))
         self.train_labels = pd.Series(score)
 
-
-
+    def create_test_label(self, test_tweet_sequence):
         score = []
         for tweet in test_tweet_sequence:
-            x = self.analyzer.polarity_scores(tweet)['compound']
-            score.append(round(x))
-
+            val = self.analyzer.polarity_scores(tweet)['compound']
+            score.append(round(val))
         self.test_labels = pd.Series(score)
 
 
 class PreProcessing:
-    def __init__(self, data_obj):
-        self.my_stopwords = list(stopwords.words('english'))
-        self.ps = PorterStemmer()
-        self.corpus = []
-        self.temp_list = []
-        self.temp_corpus = []
-        self.temp_temp_list = []
-        self.tkcorpus = []
+    def __init__(self, data_obj, processing_size=1600000):
+        data_obj.raw_tweets = data_obj.raw_tweets[: processing_size]
+
+        self.detokenized_corpus = []
+        self.tokenized_corpus = []
+
         self.remove_url(data_obj)
         self.remove_at(data_obj)
         self.remove_apostrophe(data_obj)
@@ -80,184 +66,176 @@ class PreProcessing:
         self.remove_stopwords_and_stem(data_obj)
         self.spell_correction()
 
-
     @staticmethod
     def remove_url(data_ob):
-
         data_ob.raw_tweets = data_ob.raw_tweets.str.replace('http\S+|www.\S+', '', case=False)
 
     @staticmethod
     def remove_at(data_ob):
-
         data_ob.raw_tweets = data_ob.raw_tweets.str.replace('@\S+', '', case=False)
 
     @staticmethod
     def remove_apostrophe(data_ob):
-
         data_ob.raw_tweets = data_ob.raw_tweets.str.replace("’|'\S+", '', case=False)
 
     @staticmethod
     def remove_new_line(data_ob):
-
         data_ob.raw_tweets = data_ob.raw_tweets.str.replace("\\n", '', case=False)
 
     @staticmethod
     def hyphen_to_space(data_ob):
-
         data_ob.raw_tweets = data_ob.raw_tweets.str.replace("-", ' ', case=False)
 
     @staticmethod
     def replace_emoticons(data_ob):
-
-        # data_ob.raw_tweets = pd.Series([emoji.demojize(sentence) for sentence in data_ob.raw_tweets], dtype='str')
         for i in range(len(data_ob.raw_tweets)):
             data_ob.raw_tweets[i] = emoji.demojize(data_ob.raw_tweets[i])
         data_ob.raw_tweets = data_ob.raw_tweets.str.replace("_", ' ', case=False)
 
     def remove_stopwords_and_stem(self, data_ob):
-
+        my_stopwords = list(stopwords.words('english'))
+        ps = PorterStemmer()
         for tweet in data_ob.raw_tweets:
-            self.temp_list = []
-
-            for word in TreebankWordTokenizer().tokenize(tweet):
-                if word not in self.my_stopwords:
-                    self.temp_list.append(self.ps.stem(word))
-
-
-            self.temp_corpus.append(TreebankWordDetokenizer().detokenize(self.temp_list))
+            temp_list = [ps.stem(word) for word in TreebankWordTokenizer().tokenize(tweet) if word not in my_stopwords]
+            self.tokenized_corpus.append(temp_list)
 
     def spell_correction(self):
-
-        for tweet in self.temp_corpus:
-            self.tkcorpus.append(str(TextBlob(tweet).correct()))
-            for word in TreebankWordTokenizer().tokenize(str(TextBlob(tweet).correct())):
-                self.temp_temp_list.append(word)
-            self.corpus.append(self.temp_list)
+        self.tokenized_corpus = [[str(TextBlob(word).correct()) for word in tweet] for tweet in self.tokenized_corpus]
+        self.detokenized_corpus = [TreebankWordDetokenizer().detokenize(tweet) for tweet in self.tokenized_corpus]
 
 
+class WordEmbedding:
+    def __init__(self, prep_obj, method='w2v_tfidf'):
+        self.vector_corpus = []
 
+        if method == 'w2v_tfidf':
+            self.word_to_vec(prep_obj, use_tf_idf=True)
+        elif method == 'w2v':
+            self.word_to_vec(prep_obj, use_tf_idf=False)
+        elif method == 'cv':
+            self.count_vectorizer(prep_obj)
+        elif method == 'tfidf':
+            self.tf_idf(prep_obj)
+        elif method == 'glove':
+            self.glove(prep_obj)
 
+    def count_vectorizer(self, prep_obj):
+        self.vector_corpus = CountVectorizer().fit_transform(prep_obj.detokenized_corpus)
+
+    def tf_idf(self, prep_obj):
+        vectorizer = TfidfVectorizer(lowercase=False, analyzer='word', token_pattern="\S*")
+        vec = vectorizer.fit_transform(prep_obj.detokenized_corpus).todense().tolist()
+        feature_names = vectorizer.get_feature_names()
+        self.vector_corpus = [[vec[index][feature_names.index(word)] for word in tweet]
+                              for index, tweet in enumerate(prep_obj.tokenized_corpus)]
+
+    def word_to_vec(self, prep_obj, use_tf_idf=False):
+        self.tf_idf(prep_obj)
+        tf_idf_vector = self.vector_corpus
+        self.vector_corpus = []
+
+        features = 100
+        model = gensim.models.Word2Vec(prep_obj.tokenized_corpus, min_count=1, workers=4, size=features, window=5, sg=0)
+        vec = np.zeros(features)
+        for row, tweet in enumerate(prep_obj.tokenized_corpus):
+            for column, word in enumerate(tweet):
+                mul = tf_idf_vector[row][column] if use_tf_idf else 1
+                vec = np.add(vec, np.array(model.wv[word]) * mul)
+            vec = np.true_divide(vec, 1 if len(tweet) == 0 else len(tweet))
+            self.vector_corpus.append(vec)
+
+    def glove(self, prep_obj):
+        embeddings_dict = {}
+        features = 100
+        with open("glove.twitter.27B." + str(features) + "d.txt", 'r', encoding="utf-8") as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                vector = np.asarray(values[1:], "float32")
+                embeddings_dict[word] = vector
+
+        vec = np.zeros(features)
+        self.vector_corpus = []
+        for tweet in prep_obj.tokenized_corpus:
+            for word in tweet:
+                vec = np.add(vec, np.array(embeddings_dict[word]))
+            vec = np.true_divide(vec, 1 if len(tweet) == 0 else len(tweet))
+            self.vector_corpus.append(vec)
 
 
 class Training:
-    def __init__(self, prep_obj, data_obj):
-        self.vector_corpus = []
-        self.embeddings_dict = {}
-        #self.vectorizer = CountVectorizer()
-        i=0
-        j=0
-        #self.vector_corpus = self.vectorizer.fit_transform(prep_obj.corpus[:100000])
+    def __init__(self, data_obj, prep_obj, word_embed_obj, train_size=80000, method='dt'):
+        data_obj.create_train_label(prep_obj.detokenized_corpus[: train_size])
 
-        self.model = gensim.models.Word2Vec(prep_obj.corpus, min_count=1, workers=4 ,size=100, window=5, sg=1)
-        self.vect_tfidf = None
-        self.feature = None
-        vectorizer = TfidfVectorizer()
-        self.vect_tfidf = vectorizer.fit_transform(prep_obj.tkcorpus)
+        self.X_train = word_embed_obj.vector_corpus[: train_size]
+        self.Y_train = data_obj.train_labels
 
-        self.feature = vectorizer.get_feature_names()
-        #self.words = list(self.model.wv.vocab)
-        #self.glove()
-        self.data = self.vect_tfidf
-        for word in self.feature:
-                self.embeddings_dict.update([(word,i)])
-                i+=1
-        print(self.embeddings_dict)
-        for  tweet in prep_obj.corpus:
-            le = len(tweet)+1
-            vec = np.zeros((100))
-
-            for word in tweet :
-                if word in self.embeddings_dict :
-                    u =self.embeddings_dict[word]
-                
-                    temp = self.data[j,u]
-                    vec = np.add(vec, (np.array(self.model.wv[word]) * temp))
-                else :
-                    vec = np.add(vec, np.array(self.model.wv[word])*0.001 )
-            j += 1
-
-            vec = np.true_divide(vec, le)
-            self.vector_corpus.append(vec.tolist())
-        self.vector_corpus = np.asarray(self.vector_corpus)
-        self.X = self.vector_corpus[:80000]
-        self.X_test = self.vector_corpus[80000:100000]
-        self.Y = data_obj.train_labels
         self.clf = None
-        self.y_true = data_obj.test_labels
-        self.y_pred = None
 
+        if method == 'dt':
+            self.decision_tree()
+        elif method == 'lin_svc':
+            self.linear_svc()
+        elif method == 'poly_svc':
+            self.poly_svc()
+        elif method == 'gauss_svc':
+            self.gaussian_svc()
 
-
-
-
-    def glove(self):
-
-        with open("glove.twitter.27B.100d.txt", 'r', encoding="utf-8") as f:
-            for line in f:
-                values = line.split()
-                word = values[0]
-                vector = np.asarray(values[1:], "float32")
-                self.embeddings_dict[word] = vector
-
-    def word2vec(self):
-
-        with open("glove.twitter.27B.100d.txt", 'r', encoding="utf-8") as f:
-            for line in f:
-                values = line.split()
-                word = values[0]
-                vector = np.asarray(values[1:], "float32")
-                self.embeddings_dict[word] = vector
-
-
-    def linear_SVC(self):
+    def linear_svc(self):
         self.clf = LinearSVC()
-        self.clf.fit(self.X, self.Y)
+        self.clf.fit(self.X_train, self.Y_train)
 
-    def poly_SVC(self):
+    def poly_svc(self):
         self.clf = SVC(kernel='poly', degree=3, coef0=1, C=100)
-        self.clf.fit(self.X, self.Y)
+        self.clf.fit(self.X_train, self.Y_train)
 
-    def gaussian_SVC(self):
+    def gaussian_svc(self):
         self.clf = SVC(kernel='rbf', gamma=5, C=100)
-        self.clf.fit(self.X, self.Y)
+        self.clf.fit(self.X_train, self.Y_train)
 
-    def DecisionTree(self,x):
-
+    def decision_tree(self):
         self.clf = tree.DecisionTreeClassifier()
-        self.clf.fit(self.X, self.Y)
+        self.clf.fit(self.X_train, self.Y_train)
 
+
+class Testing:
+    def __init__(self, data_obj, prep_obj, word_embed_obj, training_obj, test_size=20000):
+        train_size = len(data_obj.train_labels)
+
+        data_obj.create_test_label(prep_obj.detokenized_corpus[train_size: train_size + test_size])
+
+        self.X_test = word_embed_obj.vector_corpus[train_size:  train_size + test_size]
+        self.Y_test = data_obj.test_labels
+
+        self.clf = training_obj.clf
+        self.Y_pred = None
+
+        self.accuracy()
+        self.auc()
+        self.confusion_matrix()
 
     def accuracy(self):
-        self.y_pred = self.clf.predict(self.X_test)
-        print(accuracy_score(self.y_true, self.y_pred) * 100)
-
+        self.Y_pred = self.clf.predict(self.X_test)
+        print('Accuracy is :', accuracy_score(self.Y_test, self.Y_pred) * 100)
 
     def auc(self):
-        self.y_pred = self.clf.predict(self.X_test)
-        fpr, tpr, thresholds = metrics.roc_curve(self.y_true,self.y_pred, pos_label=1)
+        print("\nArea under curve :")
+        self.Y_pred = self.clf.predict(self.X_test)
+        fpr, tpr, thresholds = metrics.roc_curve(self.Y_test, self.Y_pred, pos_label=1)
         print("1", metrics.auc(fpr, tpr))
-        fpr, tpr, thresholds = metrics.roc_curve(self.y_true, self.y_pred, pos_label=0)
+        fpr, tpr, thresholds = metrics.roc_curve(self.Y_test, self.Y_pred, pos_label=0)
         print("0", metrics.auc(fpr, tpr))
-        fpr, tpr, thresholds = metrics.roc_curve(self.y_true,self.y_pred, pos_label=-1)
+        fpr, tpr, thresholds = metrics.roc_curve(self.Y_test, self.Y_pred, pos_label=-1)
         print("-1", metrics.auc(fpr, tpr))
 
-    def confusion(self):
-        self.y_pred = self.clf.predict(self.X_test)
-        print(confusion_matrix(self.y_true, self.y_pred))
+    def confusion_matrix(self):
+        print('\nConfusion Matrix :')
+        self.Y_pred = self.clf.predict(self.X_test)
+        print(confusion_matrix(self.Y_test, self.Y_pred))
 
 
-
-
-
-
-
-
-x = Data()
-y = PreProcessing(x)
-x.create_label(y.tkcorpus[: 80000], y.tkcorpus[80000:100000])
-z = Training(y, x)
-z.DecisionTree(x)
-z.accuracy()
-z.auc()
-z.confusion()
-
+dt = Data()
+pp = PreProcessing(dt, processing_size=1000)
+we = WordEmbedding(pp, method='w2v_tfidf')
+tr = Training(dt, pp, we, train_size=800, method='dt')
+te = Testing(dt, pp, we, tr, test_size=200)
